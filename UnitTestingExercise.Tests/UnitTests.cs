1a532bbdb68b2608837fa62fba5a3a503213d1da
Brandon Levinsky <blevinsky91@gmail.com>
2023-07-14 10:01:22 -0400

    second save

--- UnitTestingExercise.Tests/UnitTests.cs
+++ UnitTestingExercise.Tests/UnitTests.cs
@@ -14,14 +14,14 @@ namespace UnitTestingExercise.Tests
             //Arrange
             // create a Calculator object
 
-            var instanceObject = new Calculator();
+            var instObj = new Calculator();
 
 
             //Act
             // call the Add method that is located in the Calculator class
             // and store its result in a variable named actual
 
-            var actual = instanceObject.Add();
+            var actual = instObj.Add();
 
             //Assert
             //Assert.Equal(expected, actual);
@@ -30,42 +30,58 @@ namespace UnitTestingExercise.Tests
         }
 
         [Theory]
-        [InlineData()]//Add test data <-------
+        [InlineData(20, 20)]//Add test data <-------
         public void SubtractTest(int minuend, int subtrhend, int expected)
         {
             //Start Step 5 here:
 
             //Arrange
 
+            var instObj2 = new Calculator();
+
             //Act
+
+            var actual = instObj2.Subtract();
 
             //Assert
 
+            Assert.Equal(10, actual);
         }
 
         [Theory]
-        [InlineData()]//Add test data <-------
+        [InlineData(10, 2)]//Add test data <-------
         public void MultiplyTest(int num1, int num2, int expected)
         {
             //Start Step 7 here:
 
             //Arrange
 
+            var instObj3 = new Calculator();
+
             //Act
+
+            var actual = instObj3.Multiply();
 
             //Assert
 
+            Assert.Equal(10, actual);
         }
 
         [Theory]
-        [InlineData()]//Add test data <-------
+        [InlineData(20, 2)]//Add test data <-------
         public void DivideTest(int num1, int num2, int expected)
         {
             //Arrange
 
+            var instObj4 = new Calculator();
+
             //Act
 
+            var actual = instObj4.Divide();
+
             //Assert
+
+            Assert.Equal(10, actual);
 
         }
 
